e149b88ccca63dfd235ff742117db5dc02a63c88
sabrinachs <sabrinach08@yahoo.com.br>
2022-08-03 19:29:35 -0300

    Adição das funções CRUD

--- app.js
+++ app.js
@@ -33,24 +33,97 @@ app.get("/todolist", (req, res)=>{
 app.get("/todolist/:id", (req, res) =>{
     const id = req.params.id;
 
-    const task = toDoList.find(id => )
+    const task = toDoList.find(task => task.id === id);
+
+    if(!task){
+        return res.status(404).json({ errormessage: "Tarefa não encontrada." });
+    }
+    
+    res.json(task);
 
 });
 
-app.post("todolist", (req, res)=>{
-    if(!req.body){
-        return res.status(404).json({errormessage: "Tarefa não enviada no corpo da requisição"});
+app.post("/todolist", (req, res)=>{
+    if(!req.body || !Object.keys(req.body).length){
+        return res.status(404).json({ errormessage: "Tarefa não enviada no corpo da requisição" });
     }
 
     const newTask = req.body;
 
-    if(!newTask.id || !newTask.task){
-        return res.status(404).json({errormessage: "Todos os campos precisam ser preenchidos"});
+    if(!newTask.id || !newTask.tarefa || !newTask.status){
+        return res.status(400).json({ errormessage: "Todos os campos precisam ser preenchidos" });
     }
 
     toDoList.push(newTask);
     res.json(newTask);
-})
+});
+
+app.delete("/todolist/:id", (req, res)=>{
+    const id = req.params.id;
+
+    const index = toDoList.findIndex(task => task.id === id);
+
+    if(index === -1){
+        res.status(404).json({ errormessage: "Tarefa não encontrada." });
+    }
+
+    toDoList.splice(index, 1);
+    
+    res.status(200).json({ successMessage: "Tarefa removida com sucesso." });
+
+});
+
+app.put("/todolist/:id", (req, res)=>{
+    const id = req.params.id;
+
+    if(!req.body || !Object.keys(req.body).length){
+        return res.status(404).json({ errormessage: "Tarefa não enviada no corpo da requisição" });
+    }
+
+    const index = toDoList.findIndex(task => task.id === id);
+
+    if(index === -1){
+        res.status(404).json({ errormessage: "Tarefa não encontrada." });
+    }
+
+    const changedTask = req.body;
+
+    if(changedTask.id !== id){
+        res.status(402).json({ errormessage: "O id não pode ser alterado" });
+    }else if(!changedTask.id || !changedTask.tarefa || !changedTask.status){
+        return res.status(402).json({ errormessage: "Todos os campos precisam ser preenchidos." });
+    }else{
+        toDoList[index] = changedTask;
+    }
+        
+    res.json(toDoList[index]);
+
+});
+
+app.patch("/todolist/:id", (req, res)=>{
+    const id = req.params.id;
+
+    if(!req.body || !Object.keys(req.body).length){
+        return res.status(404).json({ errormessage: "Tarefa não enviada no corpo da requisição" });
+    }
+
+    const index = toDoList.findIndex(task => task.id === id);
+    
+    if(index === -1){
+        res.status(404).json({ errormessage: "Tarefa não encontrada." });
+    }
+
+    const changedTask = {...toDoList[index], ...req.body};
+
+    if(changedTask.id !== id){
+        res.status(403).json({ errormessage: "O id não pode ser alterado" });
+    }else{
+        toDoList[index] = changedTask;
+    }
+
+    res.json(toDoList[index]);
+
+});
 
 app.listen(port, () =>{
     console.log(`Server running is ${port}`);
